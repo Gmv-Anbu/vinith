3fad6eceadcbe51df7d2f7e3732dedf2b31d7b74
vinith2299 <141495701+vinith2299@users.noreply.github.com>
2023-09-16 12:10:35 +0530

    Added navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import './style.css';
 import Login from './pages/login';
 import Signup from './pages/signup';
+import Navbar from './components/navbar';
 function App() {
   return (
     <div>
       vinith
+      <Navbar></Navbar>
       <Login></Login>
       <Signup></Signup>
     </div>
