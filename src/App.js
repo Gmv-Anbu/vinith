145bf0eadc18a80b41c16274a576191e584271d6
vinith2299 <141495701+vinith2299@users.noreply.github.com>
2023-09-16 13:19:52 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import Login from './pages/login';
 import Signup from './pages/signup';
 import Calculator from './pages/Calculator';
 import Navbar from './components/navbar';
+import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <div>
       <Navbar></Navbar>
-      <Login></Login>
+      <Routes>
+        <Route path="login" element={<Login></Login>}></Route>
+        <Route path="signupsolution" element={<Signup></Signup>}></Route>
+        <Route path="calculator" element={<Calculator></Calculator>}></Route>
+      </Routes>
+      {/* <Login></Login>
       <Signup></Signup>
-      <Calculator></Calculator>
+      <Calculator></Calculator> */}
     </div>
   );
 }
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,28 +6,28 @@ function Navbar() {
     <nav>
       <ul>
         <li>
-          <a href="./login 2.html">
+          <a href="./login">
             <i className="fa-solid fa-user-secret"></i> login
           </a>
         </li>
         <li>
-          <a href="./signupsolution.html">
+          <a href="./signupsolution">
             <i classNme="fa-solid fa-right-to-bracket"></i> signup
           </a>
         </li>
         <li>
-          <a href="./calculator.html">
+          <a href="./calculator">
             <i className="fa-solid fa-calculator"></i> calculator
           </a>
         </li>
         <li>
-          <a href="./tabel.html">
+          <a href="./tabel">
             {' '}
             <i className="fa-solid fa-table"></i> tabel
           </a>
         </li>
         <li>
-          <a href="./employeeid.html">
+          <a href="./employeeid">
             <i className="fa-solid fa-id-card"></i> employee id
           </a>
         </li>
