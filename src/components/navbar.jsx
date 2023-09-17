492a6f9b169768f39f66ecfcdd2ddf600dc5df3d
vinith2299 <141495701+vinith2299@users.noreply.github.com>
2023-09-17 13:10:40 +0530

    3 files modified, 5 added

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,34 +6,54 @@ function Navbar() {
     <nav>
       <ul>
         <li>
-          <a href="./login">
+          <a href="/login">
             <i className="fa-solid fa-user-secret"></i> login
           </a>
         </li>
         <li>
-          <a href="./signupsolution">
-            <i classNme="fa-solid fa-right-to-bracket"></i> signup
+          <a href="/signupsolution">
+            <i className="fa-solid fa-right-to-bracket"></i> signup
           </a>
         </li>
         <li>
-          <a href="./calculator">
+          <a href="/calculator">
             <i className="fa-solid fa-calculator"></i> calculator
           </a>
         </li>
         <li>
-          <a href="./tabel">
+          <a href="/pagination">
+            <i className="fa-solid fa-calculator"></i> Pagination
+          </a>
+        </li>
+        <li>
+          <a href="/dummy">
+            <i className="fa-solid fa-calculator"></i> Dummy
+          </a>
+        </li>
+        {/* <li>
+          <a href="/tabel">
             {' '}
             <i className="fa-solid fa-table"></i> tabel
           </a>
-        </li>
-        <li>
-          <a href="./employeeid">
+        </li> */}
+        {/* <li>
+          <a href="/employeeid">
             <i className="fa-solid fa-id-card"></i> employee id
+          </a>
+        </li> */}
+        <li>
+          <a href="/errorpage">
+            <i className="fa-solid fa-id-card"></i> errorpage
           </a>
         </li>
         <li>
-          <a href="./errorpage">
-            <i className="fa-solid fa-id-card"></i> errorpage
+          <a href="/employeelist">
+            <i className="fa-solid fa-id-card"></i> Employeelist
+          </a>
+        </li>
+        <li>
+          <a href="/employeedetails">
+            <i className="fa-solid fa-id-card"></i> Employeedetails
           </a>
         </li>
       </ul>
