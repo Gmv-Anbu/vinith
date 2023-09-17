efa8713c5882bb6890ffabe69f02a321e7625ebf
vinith2299 <141495701+vinith2299@users.noreply.github.com>
2023-09-17 12:37:46 +0530

    1 file modified

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -2,41 +2,48 @@ import React from 'react';
 import '../css/calculator.css';
 
 function Calculator() {
+  const [result, setresult] = React.useState('2');
   const display = (key) => {
-    alert(key);
+    setresult(result + key);
   };
+  // const calculate = () => {
+  //   setresult(eval(result));
+  // };
+  // const allclear = () => {
+  //   setresult('');
+  // };
   return (
     <div>
       {' '}
       <div className="calculator">
-        <input type="text" />
+        <input type="text" value={result} disabled />
         <div>
           <button onClick={() => display('1')}>1</button>
-          <button  onClick={() => display('2')}>2</button>
-          <button  onClick={() => display('3')}>3</button>
-          <button  onClick={() => display('+')}>+</button>
+          <button onClick={() => display('2')}>2</button>
+          <button onClick={() => display('3')}>3</button>
+          <button onClick={() => display('+')}>+</button>
         </div>
         <div>
-          <button  onClick={() => display('4')}>4</button>
-          <button  onClick={() => display('5')}>5</button>
-          <button  onClick={() => display('6')}>6</button>
-          <button  onClick={() => display('-')}>-</button>
+          <button onClick={() => display('4')}>4</button>
+          <button onClick={() => display('5')}>5</button>
+          <button onClick={() => display('6')}>6</button>
+          <button onClick={() => display('-')}>-</button>
         </div>
         <div>
-          <button  onClick={() => display('7')}>7</button>
-          <button  onClick={() => display('8')}>8</button>
-          <button  onClick={() => display('9')}>9</button>
-          <button  onClick={() => display('*')}>*</button>
+          <button onClick={() => display('7')}>7</button>
+          <button onClick={() => display('8')}>8</button>
+          <button onClick={() => display('9')}>9</button>
+          <button onClick={() => display('*')}>*</button>
         </div>
         <div>
-          <button  onClick={() => display('0')}>0</button>
-          <button  onClick={() => display('/')}>/</button>
-          <button  onClick={() => display('.')}>.</button>
-          <button >C</button>
+          <button onClick={() => display('0')}>0</button>
+          <button onClick={() => display('/')}>/</button>
+          <button onClick={() => display('.')}>.</button>
+          <button>C</button>
         </div>
         <div>
-          <button>AC</button>
-          <button>=</button>
+          <button onClick={() => setresult('')}>AC</button>
+          <button onClick={() => setresult(eval(result))}>=</button>
         </div>
       </div>
     </div>
