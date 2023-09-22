c83479fff1e98e1b71e861cd801584ba30af604d
vinith2299 <141495701+vinith2299@users.noreply.github.com>
2023-09-22 16:29:52 +0530

    1 file modified

--- src/pages/errorpage.jsx
+++ src/pages/errorpage.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-import '../css/calculator.css';
 
 function Errorpage() {
-  return <div>Errorpage</div>;
-}
+  return (
+<div>
+<h1>404 - Page Not Found</h1>
+<p>We're sorry, but the page you requested could not be found.</p>
+</div>
 
+
+  );
+}
 export default Errorpage;
