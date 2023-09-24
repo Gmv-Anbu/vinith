779fd8b0c9f5b5d3c28b37167dc3a03fd0b531ac
vinith2299 <141495701+vinith2299@users.noreply.github.com>
2023-09-24 15:58:41 +0530

    1 file modified

--- src/pages/pagination.jsx
+++ src/pages/pagination.jsx
@@ -2,22 +2,20 @@ import React from 'react';
 import '../css/pagination.css';
 import Table from '../components/tabel';
 function Pagination() {
-  const data = [];
+  // const data = [];
+  const [data, setdata] = React.useState([]);
   const getdata = () => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then((response) => response.json())
       .then((responsedata) => {
         // data=responsedata;
+        setdata(responsedata);
         console.log(responsedata);
       });
   };
   getdata();
   return (
-    <div>
-      {' '}
-      {data.length >0?
-      <Table data={data}> </Table>:'loding...'}{' '}
-    </div>
+    <div> {data.length > 0 ? <Table data={data}> </Table> : 'loding...'} </div>
   );
 }
 
