1ac35f642665bf4975e024e0b8895b4e844843af
vinith2299 <141495701+vinith2299@users.noreply.github.com>
2023-09-24 15:44:41 +0530

    1 file modified

--- src/components/tabel.jsx
+++ src/components/tabel.jsx
@@ -12,15 +12,25 @@ const Table = (props) => {
     return keys.map((e) => <td>{obj[e]}</td>);
   };
   const generatrow = (data) => {
-   
-    return data.map((e) => <tr>{generatdata(props.data[0])}</tr>);
+    return data.map((e) => <tr>{generatdata(e)}</tr>);
   };
   return (
     <div>
+      <div>
+        {' '}
+        <button> &lt;&lt; </button>
+        <button> &lt; </button>
+        <button> 1 </button>
+        <button> 2 </button> 
+        <button> 3 </button>
+        <button> 4 </button>
+        <button> 5 </button>
+        <button> &gt; </button>
+        <button> &gt;&gt; </button>{' '}
+      </div>
       <table>
         <tr>{generathead(props.data[0])}</tr>
         {generatrow(props.data)}
-       
       </table>
     </div>
   );
