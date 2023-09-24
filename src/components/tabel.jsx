32100330a871b2f860a42d4c70a0ad8332e76ef4
vinith2299 <141495701+vinith2299@users.noreply.github.com>
2023-09-24 15:34:11 +0530

    1 file modified

--- src/components/tabel.jsx
+++ src/components/tabel.jsx
@@ -11,36 +11,16 @@ const Table = (props) => {
     console.log(keys);
     return keys.map((e) => <td>{obj[e]}</td>);
   };
+  const generatrow = (data) => {
+   
+    return data.map((e) => <tr>{generatdata(props.data[0])}</tr>);
+  };
   return (
     <div>
       <table>
         <tr>{generathead(props.data[0])}</tr>
-        <tr>
-          {generatdata(props.data[0])}
-        </tr>
-        <tr>
-        {generatdata(props.data[1])}
-        </tr>
-        <tr>
-        {generatdata(props.data[2])}
-        </tr>
-        <tr>
-        {generatdata(props.data[3])}
-        </tr>
-        <tr>
-          <td>5</td>
-          <td>5</td>
-          <td> swetha</td>
-          <td>swetha9@gmail.com</td>
-          <td> ntntymtgnthfeneuihberubryvbruijh</td>
-        </tr>
-        <tr>
-          <td>6</td>
-          <td>6</td>
-          <td> harshikaa</td>
-          <td>harshi24@gmail.com</td>
-          <td> tntentntnthfwhfeneuihberubryvbruijh</td>
-        </tr>
+        {generatrow(props.data)}
+       
       </table>
     </div>
   );
