4a3374ab8d39ee9479aaed40eba7a1e948325796
vinith2299 <141495701+vinith2299@users.noreply.github.com>
2023-09-30 12:53:29 +0530

    1 file modified

--- src/components/tabel.jsx
+++ src/components/tabel.jsx
@@ -4,9 +4,9 @@ const Table = (props) => {
 
   const [filterdata, setfilterdata] = React.useState([]);
 
-  const[page,setpage] = React.useState(1);
-  const[lastpage,Setpagelimit] = React.useState(1);
-  const[pagelimit,setpagelimit] = React.useState(10);
+  const [page, setpage] = React.useState(1);
+  const [lastpage, setlastpage] = React.useState(1);
+  const [pagelimit, setpagelimit] = React.useState(10);
 
   const generathead = (obj) => {
     const keys = Object.keys(obj);
@@ -26,23 +26,45 @@ const Table = (props) => {
     let start = (page - 1) * pagelimit;
     let end = start + pagelimit;
     let newdata = props.data.slice(start, end);
+    setpage(page);
     setfilterdata(newdata);
   };
+  const generatebuttons = () => {
+    let start = 1;
+    let end = lastpage;
+    let buttons = [];
+    for (let i = start; i <= end; i++) {
+      buttons.push(i);
+    }
+    return buttons.map((eachbutton) => (
+      <button onClick={() => displaypage(eachbutton)}> {eachbutton} </button>
+    ));
+  };
   React.useEffect(() => {
+    setlastpage(Math.ceil(props.data.length / pagelimit));
     displaypage(1);
   }, []);
   return (
     <div>
       <div>
-        <button onClick={()=>displaypage(1)}> &lt;&lt; </button>
-        <button onClick={()=>displaypage()}> &lt; </button>
-        <button onClick={()=>displaypage(1)}> 1 </button>
-        <button onClick={()=>displaypage(2)}> 2 </button>
-        <button onClick={()=>displaypage(3)}> 3 </button>
-        <button onClick={()=>displaypage(4)}> 4 </button>
-        <button onClick={()=>displaypage(5)}> 5 </button>
-        <button onClick={()=>displaypage()}> &gt; </button>
-        <button onClick={()=>displaypage()}> &gt;&gt; </button>
+        <button onClick={() => displaypage(1)}> &lt;&lt; </button>
+        <button onClick={() => displaypage(page - 1)} disabled={page === 1}>
+          {' '}
+          &lt;{' '}
+        </button>
+        <button onClick={() => displaypage(1)}> 1 </button>
+        <button onClick={() => displaypage(2)}> 2 </button>
+        <button onClick={() => displaypage(3)}> 3 </button>
+        <button onClick={() => displaypage(4)}> 4 </button>
+        <button onClick={() => displaypage(5)}> 5 </button>
+        <button
+          onClick={() => displaypage(page + 1)}
+          disabled={page === lastpage}
+        >
+          {' '}
+          &gt;{' '}
+        </button>
+        <button onClick={() => displaypage(lastpage)}> &gt;&gt; </button>
       </div>
       <table>
         <tr>{generathead(props.data[0])}</tr>
