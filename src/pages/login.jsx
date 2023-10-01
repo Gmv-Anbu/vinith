7ea561d0bfd15f69ceab656d26af4d1ef5e4141f
vinith2299 <141495701+vinith2299@users.noreply.github.com>
2023-10-01 13:21:43 -0700

    1 file modified

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -12,7 +12,13 @@ function Login() {
   const onsubmit = (data) => {
     // alert('hello' + name);
     console.log(data);
-    fetch('https://jsonplaceholder.typicode.com/comments')
+    fetch('https://jsonplaceholder.typicode.com/comments', {
+      method: 'POST',
+      body: JSON.stringify('data'),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
       .then((response) => response.json())
       .then((responsedata) => {
         // data=responsedata;
